7ed8395cce9254b8e2cb0ce86da530c7f3fca890
takaotokuno <sample.mail@sample.com.invalid>
2025-12-18 21:59:24 +0900

    ac: abc436 c,d

--- abc436/c/main.py
+++ abc436/c/main.py
@@ -1,7 +1,27 @@
-def main():
-    N = int(input())
-    A = list(map(int, input().split()))
-    print(sum(A))
+import sys
+input = sys.stdin.readline
 
-if __name__ == "__main__":
-    main()
+OFFSETS = ((0,0),(0,1),(1,0),(1,1))
+
+def convert_to_index(r,c):
+    return (r-1)*n + (c-1)
+
+n,m = map(int,input().split())
+used_cells = set()
+ans = 0
+
+for _ in range(m):
+    r,c = map(int,input().split())
+
+    block = set()
+    conflict = False
+    for dr,dc in OFFSETS:
+        block.add(convert_to_index(r+dr,c+dc))
+
+    if block & used_cells:
+        continue
+
+    used_cells |= block
+    ans += 1
+
+print(ans)
--- abc436/d/main.py
+++ abc436/d/main.py
@@ -1,7 +1,45 @@
-def main():
-    N = int(input())
-    A = list(map(int, input().split()))
-    print(sum(A))
+from collections import deque
 
-if __name__ == "__main__":
-    main()
+INF = 10**9
+DIRS = ((-1,0),(1,0),(0,-1),(0,1))
+
+h,w = map(int,input().split())
+labyrinth = [input().strip() for _ in range(h)]
+warp_points = [[] for _ in range(26)]
+warp_seen = [False]*26
+
+for i in range(h):
+    for j in range(w):
+        if 'a' <= labyrinth[i][j] <= 'z':
+            idx = ord(labyrinth[i][j]) - ord('a')
+            warp_points[idx].append((i,j))
+
+dp = [[INF]*w for _ in range(h)]
+dp[0][0] = 0
+
+dq = deque()
+dq.append((0,0))
+
+while dq:
+    i,j = dq.popleft()
+    for di,dj in DIRS:
+        ni,nj = i+di,j+dj
+        if 0 <= ni < h and 0 <= nj < w:
+            if labyrinth[ni][nj] != '#':
+                if dp[ni][nj] > dp[i][j] + 1:
+                    dp[ni][nj] = dp[i][j] + 1
+                    dq.append((ni,nj))
+
+    if 'a' <= labyrinth[i][j] <= 'z':
+        idx = ord(labyrinth[i][j]) - ord('a')
+        
+        if not warp_seen[idx]:
+            warp_seen[idx] = True
+
+            for ni,nj in warp_points[idx]:
+                if dp[ni][nj] > dp[i][j] + 1:
+                    dp[ni][nj] = dp[i][j] + 1
+                    dq.append((ni,nj))
+
+ans = dp[h-1][w-1] if dp[h-1][w-1] != INF else -1
+print(ans)        
