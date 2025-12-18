74250aee579b504e555f32fdcff6c53a71504349
takaotokuno <sample.mail@sample.com.invalid>
2025-12-18 23:24:45 +0900

    ac: abc436 e

--- abc436/e/main.py
+++ abc436/e/main.py
@@ -1,7 +1,23 @@
-def main():
-    N = int(input())
-    A = list(map(int, input().split()))
-    print(sum(A))
+import math
 
-if __name__ == "__main__":
-    main()
+n = int(input())
+p = list(map(int, input().split()))
+
+visited = [False] * n
+ans = 0
+
+for i in range(n):
+    if visited[i]:
+        continue
+
+    cur = i
+    size = 0
+
+    while not visited[cur]:
+        visited[cur] = True
+        size += 1
+        cur = p[cur] - 1
+
+    ans += math.comb(size, 2)
+
+print(ans)
